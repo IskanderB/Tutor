f00042916d4753dc17f5e328503b74095b1500f8
IskanderB <axurtin.rep@gmail.com>
2020-05-14 13:07:38 +0300

    Commit 6.4

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -8,9 +8,23 @@ use Illuminate\Support\Facades\App;
 class HttpsProtocol
 {
   public function handle($request, Closure $next){
-    if (!$request->secure()) {
+    if (empty($_SERVER['HTTPS']) ) {
         return redirect()->secure($request->path());
     }
     return $next($request);
   }
+
+  // public function handle($request, Closure $next)
+  //   {
+  //       if (app()->environment('local')) {
+  //           // for Proxies
+  //           Request::setTrustedProxies([$request->getClientIp()]);
+  //
+  //           if (!$request->isSecure()) {
+  //               return redirect()->secure($request->getRequestUri());
+  //           }
+  //       }
+  //
+  //       return $next($request);
+  //   }
 }
